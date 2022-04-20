9984ed6b09953fcd40ae19feefb6342963fda4ca
Frank Wagner <frank2568@gmail.com>
2022-04-20 10:35:29 +0200

    added netcore31 test project

---
+++ YaNCo_netcore31/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dbosoft.YaNco;
+
+namespace YaNCo_netcore31
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var settings = new Dictionary<string, string>
+            {
+                { "ashost", "dummy" },
+                { "sysnr", "00" },
+                { "client", "001" },
+                { "user", "dummy" },
+                { "passwd", "peng" },
+                { "lang", "EN" }
+
+            };
+
+            var rfcConnnectionBuilder = new ConnectionBuilder(settings);
+
+
+            using (var context = new RfcContext(rfcConnnectionBuilder.Build()))
+            {
+                var res = await context.CallFunction(
+                        "FUNC", f => f,
+                        f => f
+                            .HandleReturn()
+                            .MapTable("TAB", s =>
+                                from field in s.GetField<string>("FIELD")
+                                select new { field }
+                            )
+
+                    )
+
+                    .IfLeft(l => throw new Exception(l.Message));
+
+
+
+
+
+            }
+        }
+    }
+}
